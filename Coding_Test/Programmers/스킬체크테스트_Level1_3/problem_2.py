fa7a0d07102363a98a535634f7b0f667adfa0ba9
hongha0704 <hongha0704@naver.com>
2025-10-18 22:02:24 +0900

    coding test

--- Coding_Test/Programmers/스킬체크테스트_Level1_3/problem_2.py
+++ Coding_Test/Programmers/스킬체크테스트_Level1_3/problem_2.py
@@ -2,4 +2,4 @@ def solution(players, callings):
     for c in callings:
         idx = players.index(c)
         players[idx-1], players[idx] = players[idx], players[idx-1]
-    return players
+    return players 
